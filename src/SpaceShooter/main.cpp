7d157487d567c24bf3dba5cdaa4e22eb28418af3
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2024-06-07 16:13:30 +0700

    huh cat

--- src/SpaceShooter/main.cpp
+++ src/SpaceShooter/main.cpp
@@ -8,18 +8,23 @@
 
 #include <CruZPlus/GameEntity.h>
 
-// #include "GameObjects/PlayerShip.h"
-// #include "SpaceShooter/Settings.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 int main()
 {
     try
     {
-        auto shad = sf::Shader::loadFromFile("fuck", "fuck", "fuck");
-        sf::Shader::bind(&shad.value());
-        sf::Shader::bind(NULL);
-        glm::
-        
+        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+
+        // Target position (where the camera is looking at)
+        glm::vec3 targetPos = glm::vec3(0.0f, 0.0f, 0.0f);
+
+        // Up vector (defines the "up" direction of the camera)
+        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+        // Calculate the lookAt matrix
+        glm::mat4 viewMatrix = glm::lookAt(cameraPos, targetPos, up);
     }
     catch (const std::exception &e)
     {
